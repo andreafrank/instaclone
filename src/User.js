7d5ec784539a548504ed272782f1aef92b657f4f
andreafrank <andreajfrank23@gmail.com>
2020-08-31 19:58:11 -0500

    add User and faker avatar

--- src/User.js
+++ src/User.js
@@ -1,1 +1,22 @@
-User.js
+import React from 'react';
+
+const User = (props) => {
+	return (
+		<div classname="avatar">
+			<a href="/" className="avatar">
+				<img alt="avatar" src={props.avatar} />
+			</a>
+			<div>
+				<a href="/" className="author">
+					{props.author}
+				</a>
+				<div className="metadata">
+					<span className="date">{props.timeAgo}</span>
+				</div>
+				<div className="text">{props.givenComment}</div>
+			</div>
+		</div>
+	);
+};
+
+export default User;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import User from './User';
+import faker from 'faker';
+import styles from './styles.css'
 
 const App = () => {
 	return (
 		<div>
-			hi
+			<User 
+				author="hey"
+				timeAgo="what"
+				avatar={faker.image.avatar()}
+			/>
 		</div>
 	);
 };
